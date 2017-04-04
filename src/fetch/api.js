d3f6f4e3a52512c569d1e91f0e7b029d6f53d9bc
luwei <luwei@lifang.com>
2017-04-04 16:30:43 +0800

    update

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -5,7 +5,7 @@ import util from '../common/js/util'
 // axios 配置
 axios.defaults.timeout = 5000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-axios.defaults.baseURL = 'http://localhost:9108/'
+axios.defaults.baseURL = 'http://127.0.0.1:9108/'
 
 // POST传参序列化
 axios.interceptors.request.use((config) => {
