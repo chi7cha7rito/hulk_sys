1556002ebbd25c10685c3418af3978bd563f45df
Kingsley <Yu@192.168.0.105>
2017-04-06 23:49:38 +0800

    update

--- src/pages/operation/campaign.vue
+++ src/pages/operation/campaign.vue
@@ -85,7 +85,7 @@
                    width="210">
                 <template scope="scope">
                     <el-button size="small" @click="handleBuy(scope.$index, scope.row)" v-if="scope.row.match.status=='1'">重入</el-button>
-                    <el-button type="danger" size="small" @click="handleScore(scope.$index, scope.row)" v-if="scope.row.match.status=='2'">成绩</el-button>
+                    <el-button type="danger" size="small" @click="handleScore(scope.$index, scope.row)" v-if="scope.row.match.status=='2'&&!scope.row.issue">成绩</el-button>
                     <el-button type="warning" size="small" @click="handleAward(scope.$index, scope.row)" v-if="scope.row.match.result!=''">颁奖</el-button>
                 </template>
             </el-table-column>
